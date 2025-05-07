175de4b869d3aabfa5265edeba9bf6b7421e79e8
DenysNUNG <denys.paliichuk-ip231k@nung.edu.ua>
2025-05-07 22:01:54 +0300

    fix: real names & output

--- Program.cs
+++ Program.cs
@@ -8,18 +8,21 @@ class Program
     {
         var people = new List<Person>
         {
-            new Person { Name = "John Doe1", Age = 21 },
-            new Person { Name = "John Doe2", Age = 22 },
-            new Person { Name = "John Doe3", Age = 23 },
-            new Person { Name = "John Doe4", Age = 24 },
-            new Person { Name = "John Doe5", Age = 25 },
-            new Person { Name = "John Doe6", Age = 26 },
-            new Person { Name = "John Doe7", Age = 27 },
-            new Person { Name = "John Doe8", Age = 28 },
-            new Person { Name = "John Doe9", Age = 29 },
-            new Person { Name = "John Doe0", Age = 30 }
+            new Person { Name = "Tom", Age = 21 },
+            new Person { Name = "Bob", Age = 22 },
+            new Person { Name = "John", Age = 23 },
+            new Person { Name = "Jane", Age = 24 },
+            new Person { Name = "Sara", Age = 25 },
+            new Person { Name = "Martin", Age = 26 },
+            new Person { Name = "Boris", Age = 27 },
+            new Person { Name = "Sam", Age = 28 },
+            new Person { Name = "Stephen", Age = 29 },
+            new Person { Name = "Denys", Age = 30 }
         };
 
-        people.ForEach(Console.WriteLine);
+        foreach(var person in people)
+        {
+            Console.WriteLine($"Name: {person.Name} Age: {person.Age}");
+        }
     }
 }
