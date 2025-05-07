a7cc892734274b4dbe761d78c13967fe3197ff93
DenysNUNG <denys.paliichuk-ip231k@nung.edu.ua>
2025-05-07 22:08:13 +0300

    add: salary field

--- Models/Person.cs
+++ Models/Person.cs
@@ -4,4 +4,5 @@ class Person
 {
     public string Name { get; set; }
     public int Age { get; set; }
+    public double Salary {  get; set; }
 }
--- Program.cs
+++ Program.cs
@@ -8,19 +8,19 @@ class Program
     {
         var people = new List<Person>
         {
-            new Person { Name = "Tom", Age = 21 },
-            new Person { Name = "Bob", Age = 22 },
-            new Person { Name = "John", Age = 23 },
-            new Person { Name = "Jane", Age = 24 },
-            new Person { Name = "Sara", Age = 25 },
-            new Person { Name = "Martin", Age = 26 },
-            new Person { Name = "Boris", Age = 27 },
-            new Person { Name = "Sam", Age = 28 },
-            new Person { Name = "Stephen", Age = 29 },
-            new Person { Name = "Denys", Age = 30 }
+            new Person { Name = "Tom", Age = 21, Salary = 1 },
+            new Person { Name = "Bob", Age = 22, Salary = 1 },
+            new Person { Name = "John", Age = 23, Salary = 1 },
+            new Person { Name = "Jane", Age = 24, Salary = 1 },
+            new Person { Name = "Sara", Age = 25, Salary = 1 },
+            new Person { Name = "Martin", Age = 26, Salary = 1 },
+            new Person { Name = "Boris", Age = 27, Salary = 1 },
+            new Person { Name = "Sam", Age = 28, Salary = 1 },
+            new Person { Name = "Stephen", Age = 29, Salary = 1 },
+            new Person { Name = "Denys", Age = 30, Salary = 1 }
         };
 
-        foreach(var person in people)
+        foreach (var person in people)
         {
             Console.WriteLine($"Name: {person.Name} Age: {person.Age}");
         }
